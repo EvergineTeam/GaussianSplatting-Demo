90c53d4c7540281209a6d7d9929710870ed05116
David Ávila <davila@plainconcepts.com>
2025-02-24 18:04:42 +0100

    First version of the demo

---
+++ GaussianSplattingDemo/GaussianSplattingDemo.Web/wwwroot/evergine_gaussiansplatting.js
@@ -0,0 +1,39 @@
+let myWorker = new Worker("evergine_sortworker.js");
+let returnResolves = {};
+myWorker.onmessage = function (e) {
+    let cmdId = e.data[0];
+    let returnResolve = returnResolves[cmdId];
+    if (returnResolve) {
+        returnResolve(...e.data.slice(1));
+    } else {
+        console.debug(`Command return promise not found: ${cmdId}`);
+    }
+};
+
+async function runWorkerCommand(cmd, ...paramters) {
+    let cmdId = Math.random();
+    let workerSortPromise = new Promise((resolve, reject) => {
+        returnResolves[cmdId] = resolve;
+    });
+    myWorker.postMessage([cmdId, cmd, ...paramters]);
+    return await workerSortPromise;
+}
+
+async function workerInit(centersBytes, centersSize, arraySize) {
+    let splatCenters = Blazor.platform.toUint8Array(centersBytes).buffer;
+    let sortResult = new SharedArrayBuffer(arraySize * Uint32Array.BYTES_PER_ELEMENT);
+    let initPromise = runWorkerCommand('init', splatCenters, centersSize, sortResult);
+    window.workerSort = async function workerSort(cameraPosition, cameraDirection) {
+        await initPromise;
+        let result = await runWorkerCommand('sort', cameraPosition, cameraDirection);
+        window.workerSortResult = function workerSortResult() {
+            var handle = BINDING.js_typed_array_to_array(result);
+            return handle;
+        }
+    }
+
+    window.workerDispose = async function workerDispose() {
+        await initPromise;
+        let result = await runWorkerCommand('dispose');
+    }
+}
---
+++ GaussianSplattingDemo/GaussianSplattingDemo.Web/wwwroot/evergine_sortworker.js
@@ -0,0 +1,87 @@
+let startPromise = new Promise((resolve, reject) => {
+    try {
+        Module = {
+            onRuntimeInitialized: resolve
+        };
+        importScripts("gsplat_native_bundle.js");
+    } catch (e) {
+        reject(e);
+    }
+});
+
+async function main() {
+    await startPromise;
+    //console.log("Worker initilized");
+}
+
+let currentSortResult = undefined;
+
+let splats = undefined;
+let gsplatSorter = undefined;
+let orderPtr = undefined;
+let orderView = undefined;
+
+onmessage = async function (e) {
+    let cmdId = e.data[0];
+    let cmd = e.data[1];
+    try {
+        await startPromise;
+        let params = e.data.slice(2);
+        if (cmd == 'init') {
+            // Parameters: splat centers array
+            let splatCenters = params[0]; // ArrayBuffer of splat centers
+            let centersLength = params[1]; // length of centers
+            let sortResult = params[2]; // SharedArrayBuffer of sort result
+
+            splats = new Uint8Array(splatCenters);
+            orderView = new Uint8Array(sortResult);
+            arraySize = splats.length / 3;
+            
+            var splatsPtr = Module._malloc(splats.length);
+            Module.HEAPU8.set(splats, splatsPtr);
+
+            gsplatSorter = Module._InitSorter(splatsPtr, 4 * 3, centersLength);
+
+            Module._free(splatsPtr);
+
+            console.log("GSplatSorter: ", gsplatSorter);
+
+            currentSortResult = sortResult;
+            
+            orderPtr = Module._malloc(orderView.length * 4);
+
+            postMessage([cmdId]);
+        } else if (cmd == 'sort') {
+            if (!currentSortResult) {
+                throw new Error('You must call Init method before.');
+            }
+            // Parameters: camera position and direction
+            // Return: sorted splat indices
+            let camPos = params[0];
+            let camDir = params[1];
+
+            //console.time("Worker native sort");
+            Module._Sort(gsplatSorter, camPos[0], camPos[1], camPos[2], camDir[0], camDir[1], camDir[2], orderPtr, orderView.length / 4);
+            //console.timeEnd("Worker native sort");
+            
+            orderView.set(Module.HEAPU8.subarray(orderPtr, orderPtr + orderView.length));
+
+            postMessage([cmdId, orderView]);
+        } else if (cmd == 'dispose') {
+            // Release all resources
+            if (gsplatSorter) {
+                Module._DestroySorter(gsplatSorter);
+                gsplatSorter = undefined;
+
+                Module._free(orderPtr);
+            }
+
+            postMessage([cmdId, "Resources disposed"]);
+        }
+    } catch (e) {
+        console.error(`Worker command error ${cmd}: ${e}`, e, e.stack);
+        throw e;
+    }
+}
+
+main();
